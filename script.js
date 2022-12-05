165f3ecc045f8cf8b23951894728b5242ae7e9d8
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-05 15:53:22 -0300

    game board check

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function render() {
-  console.log("akkk");
+  const gameBoard = document.getElementById("ticTacToeGrid");
+  gameBoard.innerHTML = "";
+  let gameBoardElement = [];
+  for (let i = 1; i < 4; i++) {
+    for (let j = 1; j < 4; j++) {
+      let aSquare = document.createElement("div");
+      aSquare.id = `square${i}${j}`;
+      aSquare.className = "squareSize";
+      gameBoardElement.push(aSquare);
+    }
+  }
+  gameBoardElement.forEach((element) => {
+    gameBoard.append(element);
+  });
 }
