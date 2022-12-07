de9c304c2e0da74364bd0a45beabffb8bcdc324b
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-07 11:10:41 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tic Tac Toe
  JavaScript Course from the odin project
+
+https://yokuny.github.io/Tic-Tac-Toe/
--- script.js
+++ script.js
@@ -24,21 +24,22 @@ function gameBoardRender() {
     gameBoard.append(element);
   });
 }
-const player = (name, xOrSquare) => {
+const player = (name, xOrSquare, iaMode) => {
   let winCount = 0;
+  const winIncrease = (winCount) => winCount++;
+  z;
   if (name == "") {
     name = "Player";
+  } else if (iaMode) {
+    name = "IA";
   }
-  return { name, xOrSquare, winCount };
+  return { name, xOrSquare, winCount, winIncrease };
 };
 function createPlayers(player1Name, player2Name, iaMode) {
   const firstPlayer = player(player1Name, "X", iaMode);
   const secondPlayer = player(player2Name, "O", iaMode);
-
-  if (iaMode) {
-  } else {
-  }
-
+  console.log(firstPlayer.winIncrease());
+  //Display de Nomes dos jogadores na tela
   const firstPlayerDOMElement = document.getElementById("firstPlayerName");
   firstPlayerDOMElement.textContent = `${firstPlayer.name} - ${firstPlayer.xOrSquare}`;
   const secondPlayerDOMElement = document.getElementById("secondPlayerName");
@@ -52,4 +53,23 @@ function registerScreen() {
   renderRegistration.style = "display:none;";
   createPlayers(firstPlayerInput, secondPlayerInput, vsIA);
   gameBoardRender();
+}
+
+function funcao() {
+  const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
+
+  calculator.add(3, 5); // 8
+  calculator.sub(6, 2); // 4
+  calculator.mul(14, 5534); // 77476
 }
