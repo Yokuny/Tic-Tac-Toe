f664c276c655ec5f3e8887fa3877b3607eef5a06
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-09 10:51:33 -0300

    game core is done

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let firstPlayer, secondPlayer;
-let currentPlayer;
-
 const player = (playerName, playerxo, iaMode) => {
   let name = playerName;
   if (name == "") {
@@ -16,13 +13,43 @@ const player = (playerName, playerxo, iaMode) => {
   const getWinAmount = () => winCount;
   return { getPlayerName, getXO, winIncrease, getWinAmount };
 };
-
 const core = (() => {
   const gameBoardClear = () => {
     document.getElementById("ticTacToeGrid").innerHTML = "";
   };
   const closeRegisterScreen = () => {
     document.getElementById("floatingRegisterDisplay").style = "display:none;";
+  };
+  const idsArray = () => {
+    const squareIds = Array(9);
+    let index = 0;
+    for (let i = 1; i < 4; i++) {
+      for (let j = 1; j < 4; j++) {
+        squareIds[index] = `${i}${j}`;
+        index++;
+      }
+    }
+    return squareIds;
+  };
+  const squareElement = (elementsId) => {
+    let squareElement = [];
+    elementsId.forEach((element) => {
+      let aSquare = document.createElement("div");
+      aSquare.style = `grid-area: a${element}`;
+      aSquare.className = "squareSize";
+      aSquare.id = `square${element}`;
+      aSquare.innerText = `-`;
+      if (element == "x" || element == "X") {
+        aSquare.innerText = `X`;
+      } else if (element == "o" || element == "O") {
+        aSquare.innerText = `O`;
+      }
+      aSquare.addEventListener("click", function () {
+        play(`${this.id}`);
+      });
+      squareElement.push(aSquare);
+    });
+    return { squareElement };
   };
   const gameBoardRender = (boardGridElements) => {
     const gameBoard = document.getElementById("ticTacToeGrid");
@@ -38,72 +65,44 @@ const core = (() => {
       "secondPlayerName"
     ).textContent = `${player2.getPlayerName()} - ${player2.getXO()}`;
   };
-  const idsArray = () => {
-    const squareIds = Array(9);
-    let index = 0;
-    for (let i = 1; i < 4; i++) {
-      for (let j = 1; j < 4; j++) {
-        squareIds[index] = `${i}${j}`;
-        index++;
-      }
-    }
-    return squareIds;
-  };
-  const boardElements = (elementsId) => {
-    let boardElements = [];
-    elementsId.forEach((element) => {
-      let aSquare = document.createElement("div");
-      aSquare.style = `grid-area: a${element}`;
-      aSquare.className = "squareSize";
-      aSquare.id = `square${element}`;
-      aSquare.innerText = `-`;
-      if (element == "x" || element == "X") {
-        aSquare.innerText = `X`;
-      } else if (element == "o" || element == "O") {
-        aSquare.innerText = `O`;
-      }
-      aSquare.addEventListener("click", function () {
-        play(`${this.id}`);
-      });
-      boardElements.push(aSquare);
-    });
-    return { boardElements };
-  };
-  const boardElementsRender = (boardElements) => {
-    boardElements.boardElements.forEach((element) => {
-      document.getElementById("ticTacToeGrid").appendChild(element);
-    });
-  };
   return {
     gameBoardClear,
-    gameBoardRender,
     closeRegisterScreen,
-    displayPlayersName,
     idsArray,
-    boardElements,
-    boardElementsRender,
+    squareElement,
+    gameBoardRender,
+    displayPlayersName,
   };
 })();
 
+let firstPlayer;
+let secondPlayer;
+let currentPlayer;
+
+function checkCurrentPlayer() {
+  if (actualPlayer.xOrSquare == "O") {
+    actualPlayer;
+  }
+}
 function play(id) {
   const square = document.getElementById(id);
   square.textContent = "X";
 }
+
 function start() {
-  function createPlayers(name1, name2, iaMode) {
-    const firstPlayer = player(name1, "X", iaMode);
-    const secondPlayer = player(name2, "O", iaMode);
-  }
-  createPlayers(
+  core.gameBoardClear();
+  core.closeRegisterScreen();
+  const squares = core.squareElement(core.idsArray());
+  core.gameBoardRender(squares.squareElement);
+  firstPlayer = player(
     document.getElementById("firstPlayer").value,
-    document.getElementById("secondPlayer").value,
+    "X",
     document.getElementById("iaYes").checked
   );
-
-  function currentPlayer() {}
-  function checkCurrentPlayer() {
-    if (actualPlayer.xOrSquare == "O") {
-      actualPlayer;
-    }
-  }
+  secondPlayer = player(
+    document.getElementById("secondPlayer").value,
+    "O",
+    document.getElementById("iaYes").checked
+  );
+  core.displayPlayersName(firstPlayer, secondPlayer);
 }
