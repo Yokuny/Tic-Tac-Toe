5db5f5e8a13157960ce38e94b21547d56f9f9a05
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-09 10:50:48 -0300

    working in a game core engine

--- script.js
+++ script.js
@@ -1,68 +1,109 @@
-/*
-function currentPlayer() {}
-function checkCurrentPlayer() {
-  if (actualPlayer.xOrSquare == "O") {
-    actualPlayer;
-  }
-}
-*/
+let firstPlayer, secondPlayer;
+let currentPlayer;
 
-const playChoice = (id) => {
-  const square = document.getElementById(id);
-  square.textContent = "X";
-};
-
-const core = (() => {
-  const gameBoardRender = () => {
-    const gameBoard = document.getElementById("ticTacToeGrid");
-    gameBoard.innerHTML = "";
-    let gameBoardElement = [];
-    for (let i = 1; i < 4; i++) {
-      for (let j = 1; j < 4; j++) {
-        let aSquare = document.createElement("div");
-        aSquare.style = `grid-area: a${i}${j}`;
-        aSquare.className = "squareSize";
-        aSquare.id = `square${i}${j}`;
-        aSquare.innerText = "-";
-        aSquare.addEventListener("click", function () {
-          playChoice(`${this.id}`);
-        });
-        gameBoardElement.push(aSquare);
-      }
-    }
-    gameBoardElement.forEach((element) => {
-      gameBoard.append(element);
-    });
-  };
-  const registerScreen = () => {
-    document.getElementById("floatingRegisterDisplay").style = "display:none;";
-    createPlayers(
-      document.getElementById("firstPlayer").value,
-      document.getElementById("secondPlayer").value,
-      document.getElementById("iaYes").checked
-    );
-  };
-  const displayPlayersName = (player1, player2) => {
-    document.getElementById("firstPlayerName").textContent = `${player1.name} - ${player1.xo}`;
-    document.getElementById("secondPlayerName").textContent = `${player2.name} - ${player2.xo}`;
-  };
-  return { gameBoardRender, registerScreen, displayPlayersName };
-})();
-
-const player = (name, xo, iaMode) => {
-  let winCount = 0;
-  const winIncrease = () => winCount++;
-  const getWinAmount = () => winCount;
+const player = (playerName, playerxo, iaMode) => {
+  let name = playerName;
   if (name == "") {
     name = "Player";
   } else if (iaMode) {
     name = "IA";
   }
-  return { name, xo, winIncrease, getWinAmount };
+  const getPlayerName = () => name;
+  let xo = playerxo;
+  const getXO = () => xo;
+  let winCount = 0;
+  const winIncrease = () => winCount++;
+  const getWinAmount = () => winCount;
+  return { getPlayerName, getXO, winIncrease, getWinAmount };
 };
 
-function createPlayers(name1, name2, iaMode) {
-  const firstPlayer = player(name1, "X", iaMode);
-  const secondPlayer = player(name2, "O", iaMode);
-  core.displayPlayersName(firstPlayer, secondPlayer);
+const core = (() => {
+  const gameBoardClear = () => {
+    document.getElementById("ticTacToeGrid").innerHTML = "";
+  };
+  const closeRegisterScreen = () => {
+    document.getElementById("floatingRegisterDisplay").style = "display:none;";
+  };
+  const gameBoardRender = (boardGridElements) => {
+    const gameBoard = document.getElementById("ticTacToeGrid");
+    boardGridElements.forEach((element) => {
+      gameBoard.append(element);
+    });
+  };
+  const displayPlayersName = (player1, player2) => {
+    document.getElementById(
+      "firstPlayerName"
+    ).textContent = `${player1.getPlayerName()} - ${player1.getXO()}`;
+    document.getElementById(
+      "secondPlayerName"
+    ).textContent = `${player2.getPlayerName()} - ${player2.getXO()}`;
+  };
+  const idsArray = () => {
+    const squareIds = Array(9);
+    let index = 0;
+    for (let i = 1; i < 4; i++) {
+      for (let j = 1; j < 4; j++) {
+        squareIds[index] = `${i}${j}`;
+        index++;
+      }
+    }
+    return squareIds;
+  };
+  const boardElements = (elementsId) => {
+    let boardElements = [];
+    elementsId.forEach((element) => {
+      let aSquare = document.createElement("div");
+      aSquare.style = `grid-area: a${element}`;
+      aSquare.className = "squareSize";
+      aSquare.id = `square${element}`;
+      aSquare.innerText = `-`;
+      if (element == "x" || element == "X") {
+        aSquare.innerText = `X`;
+      } else if (element == "o" || element == "O") {
+        aSquare.innerText = `O`;
+      }
+      aSquare.addEventListener("click", function () {
+        play(`${this.id}`);
+      });
+      boardElements.push(aSquare);
+    });
+    return { boardElements };
+  };
+  const boardElementsRender = (boardElements) => {
+    boardElements.boardElements.forEach((element) => {
+      document.getElementById("ticTacToeGrid").appendChild(element);
+    });
+  };
+  return {
+    gameBoardClear,
+    gameBoardRender,
+    closeRegisterScreen,
+    displayPlayersName,
+    idsArray,
+    boardElements,
+    boardElementsRender,
+  };
+})();
+
+function play(id) {
+  const square = document.getElementById(id);
+  square.textContent = "X";
+}
+function start() {
+  function createPlayers(name1, name2, iaMode) {
+    const firstPlayer = player(name1, "X", iaMode);
+    const secondPlayer = player(name2, "O", iaMode);
+  }
+  createPlayers(
+    document.getElementById("firstPlayer").value,
+    document.getElementById("secondPlayer").value,
+    document.getElementById("iaYes").checked
+  );
+
+  function currentPlayer() {}
+  function checkCurrentPlayer() {
+    if (actualPlayer.xOrSquare == "O") {
+      actualPlayer;
+    }
+  }
 }
