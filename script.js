c4b3a8bf0698c1b139774e990adb54bea292150f
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-10 23:48:45 -0300

    winning check and displace are made

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const player = (playerName, playerxo, iaMode) => {
   const getWinAmount = () => winCount;
   return { getPlayerName, getXO, winIncrease, getWinAmount };
 };
+
 const core = (() => {
   const gameBoardClear = () => {
     document.getElementById("ticTacToeGrid").innerHTML = "";
@@ -65,6 +66,61 @@ const core = (() => {
       "secondPlayerName"
     ).textContent = `${player2.getPlayerName()} - ${player2.getXO()}`;
   };
+  const changeCurrentPlayer = (xo) => {
+    if (xo == "X" || xo == "x") {
+      currentPlayer = secondPlayer;
+    } else {
+      currentPlayer = firstPlayer;
+    }
+  };
+  function winnerDisplay(id1, id2, id3) {
+    document.getElementById(`square${id1}`).style.backgroundColor = "#00ec3b";
+    document.getElementById(`square${id2}`).style.backgroundColor = "#00ec3b";
+    document.getElementById(`square${id3}`).style.backgroundColor = "#00ec3b";
+  }
+  function sequenceCheck(a1, a2, a3) {
+    if (a1 == "-" || a2 == "-" || a3 == "-") {
+      return false;
+    } else {
+      return a1 == a2 && a1 == a3;
+    }
+  }
+  const winnerCheck = () => {
+    const square11 = document.getElementById("square11").innerHTML;
+    const square12 = document.getElementById("square12").innerHTML;
+    const square13 = document.getElementById("square13").innerHTML;
+    const square21 = document.getElementById("square21").innerHTML;
+    const square22 = document.getElementById("square22").innerHTML;
+    const square23 = document.getElementById("square23").innerHTML;
+    const square31 = document.getElementById("square31").innerHTML;
+    const square32 = document.getElementById("square32").innerHTML;
+    const square33 = document.getElementById("square33").innerHTML;
+    if (sequenceCheck(square11, square12, square13)) {
+      winnerDisplay(11, 12, 13);
+      return true;
+    } else if (sequenceCheck(square21, square22, square32)) {
+      winnerDisplay(21, 22, 32);
+      return true;
+    } else if (sequenceCheck(square31, square32, square33)) {
+      winnerDisplay(31, 32, 33);
+      return true;
+    } else if (sequenceCheck(square11, square21, square31)) {
+      winnerDisplay(11, 21, 31);
+      return true;
+    } else if (sequenceCheck(square12, square22, square32)) {
+      winnerDisplay(12, 22, 32);
+      return true;
+    } else if (sequenceCheck(square13, square23, square33)) {
+      winnerDisplay(13, 23, 33);
+      return true;
+    } else if (sequenceCheck(square11, square22, square33)) {
+      winnerDisplay(11, 22, 33);
+      return true;
+    } else if (sequenceCheck(square31, square22, square13)) {
+      winnerDisplay(31, 22, 13);
+      return true;
+    } else return;
+  };
   return {
     gameBoardClear,
     closeRegisterScreen,
@@ -72,6 +128,8 @@ const core = (() => {
     squareElement,
     gameBoardRender,
     displayPlayersName,
+    changeCurrentPlayer,
+    winnerCheck,
   };
 })();
 
@@ -79,14 +137,12 @@ let firstPlayer;
 let secondPlayer;
 let currentPlayer;
 
-function checkCurrentPlayer() {
-  if (actualPlayer.xOrSquare == "O") {
-    actualPlayer;
-  }
-}
 function play(id) {
   const square = document.getElementById(id);
-  square.textContent = "X";
+  if (square.innerHTML != "-") return;
+  if (core.winnerCheck()) return;
+  square.textContent = currentPlayer.getXO();
+  core.changeCurrentPlayer(currentPlayer.getXO());
 }
 
 function start() {
@@ -100,5 +156,6 @@ function start() {
     "O",
     document.getElementById("iaYes").checked
   );
+  currentPlayer = firstPlayer;
   core.displayPlayersName(firstPlayer, secondPlayer);
 }
